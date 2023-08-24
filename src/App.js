bc6993a99b7567cb8c358be90cfe7c20d95fb05b
farouk527 <79808552+farouk527@users.noreply.github.com>
2023-08-24 20:17:15 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Form, Input, TextArea, Button, Image } from 'semantic-ui-react';
+
+import SingleFunctionApp from './CustomizableForm';
+function App() {
+  
+
+  return (
+    <div>
+      <SingleFunctionApp/>
+    </div>
+  );
+};
+
+
+export default App;
